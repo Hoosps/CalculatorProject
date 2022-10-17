8535f350c9d556600508c3438464faa120c0cb0f
Gurt <110954303+Hoosps@users.noreply.github.com>
2022-10-17 12:43:50 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,8 +4,31 @@ public class Calculator {
   public static void main(String[] args) {
     
     
+      
     
     
   }
-
+  public static double add(double a, double b) {
+    return a + b;
+   
+  }
+  public static double sub(double a, double b) {
+    return a - b;
+  }
+  public static double multiply(double a, double b) {
+    return a * b;
+   
+  }
+  public static double divide(double a, double b) {
+    return a / b;
+   
+  }
+  public static double remainder(double a, double b) {
+    return a % b;
+   
+  }
+  public static double randomNum(double max, double min) {
+    int randNum;
+   
+  }
 }
